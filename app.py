42bea62bd277ad2c2d0702b6ee567c3f3d3eecda
TuckerArrants <68343441+TuckerArrants@users.noreply.github.com>
2025-04-04 17:33:01 -0700

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ if df_1h is not None:
     instrument_options = df_1h['Instrument'].dropna().unique().tolist()
     selected_instrument = st.sidebar.selectbox("Select Instrument", instrument_options)
     hour_options = ['All'] + list(range(0, 24))
-    three_hour_options = ['All'] + ["0", "3", "6", "9", "12", "15", "18", "21"]
+    three_hour_options = ['All'] + [0, 3, 6, 9, 12, 15, 18, 21]
     selected_hour = st.sidebar.selectbox("Select Hour", hour_options)
     selected_three_hour = st.sidebar.selectbox("Select 3H", three_hour_options)
     day_options = ['All'] + ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
